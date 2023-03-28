13f5b6023de0202122b6b9568f6daa3f3a146256
Lauren Golden <llgolden@gmail.com>
2023-03-28 14:41:42 -0600

    Completed practice assignment

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,35 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron_name) 
+    @patrons << patron_name
+  end
+
+  def yell_at_patrons 
+    patrons_upper = []
+    patrons.each do |patron|
+      patrons_upper << patron.upcase
+    end
+    patrons_upper
+  end
+
+  def over_capacity?
+    if patrons.length <=4
+      false
+    else
+      true
+    end 
+  end
+
+  def kick_out
+    while over_capacity? == true
+      @patrons.pop
+    end
+  end
 end
