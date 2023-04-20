5f8444f19084842bf5ca14c40e3e976e538af37f
Lauren Golden <llgolden@gmail.com>
2023-04-19 18:37:11 -0600

    updated methods

--- practice_independent_challenges/week5/museo/lib/curator.rb
+++ practice_independent_challenges/week5/museo/lib/curator.rb
@@ -20,14 +20,64 @@ class Curator
     end
   end
 
+  # def photographs_by_artist
+  #   artists_photos = {} 
+  #   @photographs.select do |photo|
+  #       if artists_photos[photo.artist_id]
+  #         artists_photos[photo.artist_id] = [photo] << photo
+  #       else
+  #         artists_photos[photo.artist_id] = [photo]
+  #       end
+  #   end
+  #   @artists.select do |artist|
+  #     artists_photos.select do |photo|
+  #       if photo == artist.id
+  #         artists_photos[artist.id] = artists_photos.delete photo
+  #       end
+  #     end
+  #   end
+  #   artists_photos
+  #   require 'pry'; binding.pry
+  # end
+
+  # def photographs_by_artist
+  #   artists_photos = {}
+  #   @artists.each do |artist|
+  #     artists_photos[artist] = []
+  #   end
+  #   artist1 = []
+  #   artist2 = []
+  #   artist3 = []
+  #   @photographs.each do |photo|
+  #     if photo.artist_id == "1"
+  #       artist1.push(photo)
+  #     elsif photo.artist_id == "2"
+  #       artist2.push(photo)
+  #     else 
+  #       artist3.push(photo)
+  #       require 'pry'; binding.pry 
+  #     end
+  #   end
+  # end
+
   def photographs_by_artist
-    {
-      artist => [photo]
-    }
+    artists_photos = {}
+    @photographs.each do |photo|
+      artist = @artists.find { |a| a.id == photo.artist_id }
+      artists_photos[artist] ||= []
+      artists_photos[artist] << photo
+    end
+    artists_photos
+  end
+
+    # hash[:new_key] = hash.delete :old_key
+
 # iterate through the array of artists and each artist object = key
 # iterate through the array of photographs and 
 
-  end
+def artists_with_multiple_photographs
+
+end
 
 
 end #final
--- practice_independent_challenges/week5/museo/spec/curator_spec.rb
+++ practice_independent_challenges/week5/museo/spec/curator_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe Curator do
   end
 
   describe "can search the collection" do
-    before(:each) do
+    it "can search for photographs by artists" do
       curator = Curator.new
       photo_1 = Photograph.new({
         id: "1",      
@@ -115,9 +115,7 @@ RSpec.describe Curator do
       curator.add_photograph(photo_2)
       curator.add_photograph(photo_3)
       curator.add_photograph(photo_4)
-    end
 
-    it "can search for photographs by artists" do
       expect(curator.photographs_by_artist).to eq({
         artist_1 => [photo_1],
         artist_2 => [photo_2],
@@ -126,6 +124,60 @@ RSpec.describe Curator do
     end
 
     xit "can search for artists with multiple photographs" do
+      curator = Curator.new
+      photo_1 = Photograph.new({
+        id: "1",      
+        name: "Rue Mouffetard, Paris (Boy with Bottles)",      
+        artist_id: "1",      
+        year: "1954"      
+      })        
+      photo_2 = Photograph.new({    
+        id: "2",      
+        name: "Moonrise, Hernandez",      
+        artist_id: "2",      
+        year: "1941"      
+      })        
+      photo_3 = Photograph.new({    
+        id: "3",      
+        name: "Identical Twins, Roselle, New Jersey",      
+        artist_id: "3",      
+        year: "1967"      
+      })        
+      photo_4 = Photograph.new({    
+        id: "4",      
+        name: "Monolith, The Face of Half Dome",      
+        artist_id: "3",      
+        year: "1927"      
+      })        
+      artist_1 = Artist.new({    
+        id: "1",      
+        name: "Henri Cartier-Bresson",      
+        born: "1908",      
+        died: "2004",      
+        country: "France"      
+      })        
+      artist_2 = Artist.new({    
+        id: "2",      
+        name: "Ansel Adams",      
+        born: "1902",      
+        died: "1984",      
+        country: "United States"      
+      })        
+      artist_3 = Artist.new({    
+        id: "3",      
+        name: "Diane Arbus",      
+        born: "1923",      
+        died: "1971",      
+        country: "United States"      
+      })        
+      curator.add_artist(artist_1)  
+      curator.add_artist(artist_2)    
+      curator.add_artist(artist_3)
+      curator.add_photograph(photo_1)
+      curator.add_photograph(photo_2)
+      curator.add_photograph(photo_3)
+      curator.add_photograph(photo_4)
+
       expect(curator.artists_with_multiple_photographs).to eq(["Diane Arbus"])
     end 
 
